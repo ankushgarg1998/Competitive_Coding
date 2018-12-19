cd9f654eee07d19861fc0b9a624223ff046188c6
ankushgarg1998 <1998anky@gmail.com>
2018-12-20 01:58:55 +0530

    Events

---
+++ JAVA Lab/MouseEvents/MouseEvents.java
@@ -0,0 +1,71 @@
+import java.awt.Graphics;
+import java.awt.event.*;
+import java.applet.*;
+
+/*
+    <applet code="MouseEvents" width=300 height=100>
+    </applet>
+*/
+
+public class MouseEvents extends Applet implements MouseListener, MouseMotionListener {
+    String msg = "";
+    int mouseX = 0, mouseY = 0;
+
+    public void init() {
+        addMouseListener(this);
+        addMouseMotionListener(this);
+    }
+
+    public void mouseClicked(MouseEvent me) {
+        mouseX = 0;
+        mouseY = 10;
+        msg = "Mouse Clicked";
+        repaint();
+    }
+
+    public void mouseEntered(MouseEvent me) {
+        mouseX = 0;
+        mouseY = 10;
+        msg = "Mouse Entered";
+        repaint();
+    }
+
+    public void mouseExited(MouseEvent me) {
+        mouseX = 0;
+        mouseY = 10;
+        msg = "Mouse Exited";
+        repaint();
+    }
+
+    public void mousePressed(MouseEvent me) {
+        mouseX = me.getX();
+        mouseY = me.getY();
+        msg = "Mouse Down";
+        repaint();
+    }
+
+    public void mouseReleased(MouseEvent me) {
+        mouseX = me.getX();
+        mouseY = me.getY();
+        msg = "Mouse UP";
+        repaint();
+    }
+
+    public void mouseDragged(MouseEvent me) {
+        mouseX = me.getX();
+        mouseY = me.getY();
+        msg = "*";
+        showStatus("Dragging mouse at " + mouseX + ", " + mouseY);
+        repaint();
+    }
+
+    public void mouseMoved(MouseEvent me) {
+        mouseX = me.getX();
+        mouseY = me.getY();
+        showStatus("Moving mouse at " + mouseX + ", " + mouseY);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(msg, mouseX, mouseY);
+    }
+}
---
+++ JAVA Lab/applet/HelloWorld.java
@@ -0,0 +1,22 @@
+// A Hello World Applet 
+// Save file as HelloWorld.java 
+  
+import java.applet.Applet; 
+import java.awt.Graphics; 
+  
+/* 
+<applet code="HelloWorld" width=200 height=60> 
+</applet> 
+*/
+  
+// HelloWorld class extends Applet 
+public class HelloWorld extends Applet  
+{ 
+    // Overriding paint() method 
+    @Override
+    public void paint(Graphics g)  
+    { 
+        g.drawString("Hello World Ji", 0, 25); 
+    } 
+      
+} 
