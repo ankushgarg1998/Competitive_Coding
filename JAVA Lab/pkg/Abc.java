7c1f59f8952a0858b5d50cd9e9f6317e32735923
ankushgarg1998 <1998anky@gmail.com>
2018-12-20 01:57:28 +0530

    pkg

---
+++ JAVA Lab/pkg/Abc.java
@@ -0,0 +1,10 @@
+package pkg;
+
+public class Abc {
+    public void show() {
+        System.out.println("hi - pkg");
+    }
+    public static void main(String args[]) {
+        System.out.println("hi");
+    }
+}
---
+++ JAVA Lab/pkg/Abcd.java
@@ -0,0 +1,10 @@
+package pack1;
+import pkg.Abc;
+
+public class Abcd extends Abc {
+    public static void main(String args[]) {
+        System.out.println("hi");
+        Abc a = new Abc();
+        a.show();
+    }
+}
