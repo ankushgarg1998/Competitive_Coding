ede4997e5ad7c1e3c96b349e80a21af4e5f8211b
ankushgarg1998 <1998anky@gmail.com>
2019-01-24 18:55:31 +0530

    temps updated

--- CodeForces/temp/second.cpp
+++ CodeForces/temp/second.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define lli long long int
 #define li long int
 #define MOD 1000000007
@@ -8,8 +9,7 @@
 #define loopr(i,a,b) for(int i=a;i>=b;i--)
 using namespace std;
 
-int main()
-    {
+int main() {
     
     return 0;
-    }
+}
