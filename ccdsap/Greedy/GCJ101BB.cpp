2ace424c7e75202a8b09c0bd589f524550937d3c
ankushgarg1998 <1998anky@gmail.com>
2019-03-23 16:52:55 +0530

    SPOJ more

--- ccdsap/Greedy/GCJ101BB.cpp
+++ ccdsap/Greedy/GCJ101BB.cpp
@@ -9,7 +9,34 @@
 #define loopr(i,a,b) for(int i=a;i>=b;i--)
 using namespace std;
 
-int main() { 
-    
+int main() {
+    int tes;
+    cin>>tes;
+    loop(te, 1, tes+1) {
+        cout<<"Case #"<<te<<": ";
+        lli n, k, b, t;
+        cin>>n>>k>>b>>t;
+        long double x[n], v[n], a[n];
+        loop(i, 0, n)
+            cin>>x[i];
+        loop(i, 0, n) {
+            cin>>v[i];
+            a[i] = (b-x[i])/v[i];
+        }
+        lli blocks=0, chicks=0, ansb=0;
+        loopr(i, n-1, 0) {
+            if(a[i] <= t) {
+                ++chicks;
+                ansb += blocks;
+                if(chicks >= k)
+                    break;
+            } else
+                ++blocks;
+        }
+        if(chicks >= k)
+            cout<<ansb<<"\n";
+        else
+            cout<<"IMPOSSIBLE\n";
+    }
     return 0;
 }
